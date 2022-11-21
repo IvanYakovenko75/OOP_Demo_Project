c256f231ee75801309fe05f664353907c8f9f9ac
Ivan <ivanyakovenko75@gmail.com>
2022-11-21 14:11:09 +0200

    Quit
    Quit implemented

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class SceneManager : MonoBehaviour
+{
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); 
+#endif
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
